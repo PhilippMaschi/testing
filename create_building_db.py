1598df1699ad176c755775b821335b139663e623
PhilippMaschi <phil.masch@hotmail.com>
2022-02-25 18:19:10 +0100

    create building data json works

--- create_building_db.py
+++ create_building_db.py
@@ -40,6 +40,76 @@ country_list = ['AUT',
                 'SWE',
                 'GBR']
 
+heating_key = {
+    1: "no heating",
+    2: "no heating",
+    3: "district heating",
+    4: "district heating",
+    5: "district heating",
+    6: "district heating",
+    7: "district heating",
+    8: "district heating",
+    9: "oil",
+    10: "oil",
+    11: "oil",
+    12: "oil",
+    13: "oil",
+    14: "oil",
+    15: "oil",
+    16: "oil",
+    17: "oil",
+    18: "coal",
+    19: "coal",
+    20: "coal",
+    21: "gas",
+    22: "gas",
+    23: "gas",
+    24: "gas",
+    25: "gas",
+    26: "gas",
+    27: "gas",
+    28: "gas",
+    29: "wood",
+    30: "wood",
+    31: "wood",
+    32: "wood",
+    33: "wood",
+    34: "wood",
+    35: "wood",
+    36: "wood",
+    37: "electricity",
+    38: "electricity",
+    39: "electricity",
+    40: "split system",
+    41: "split system",
+    42: "heat pump air",
+    43: "heat pump ground",
+    44: "electricity"
+    }
+
+df_all_countries = pd.DataFrame(columns=[
+    "index",
+    "name",
+    "construction_period_start",
+    "construction_period_end",
+     "bc_index",
+    "building_categories_index",
+    "Af",
+    "Hop",
+    "Htr_w",
+    "Hve",
+    "CM_factor",
+    "Am_factor",
+    "average_effective_area_wind_west_east_red_cool",
+    "average_effective_area_wind_south_red_cool",
+    "average_effective_area_wind_north_red_cool",
+    "spec_int_gains_cool_watt",
+    "hwb_norm",
+    "number_of_buildings",
+    "number_of_buildings_with_HP_ground",
+    "number_of_buildings_with_HP_air",
+    "country"])
+
 for country in country_list:
 
     sub_folder_name = "_scen_"+ country.lower() + "_electrification_ref_final_ab/ADD_RESULTS/"
@@ -108,10 +178,12 @@ for country in country_list:
 
     # get the total number of buildings for each bc index and the total number 
     # of buildings using a heat pump
-    building_df["number_of_buildings"] = 0
-    building_df["number_of_buildings_with_HP_ground"] = 0
-    building_df["number_of_buildings_with_HP_air"] = 0
-    bc_indices = building_df.loc[:, "index"].to_numpy()
+    building_df.loc[:, "number_of_buildings"] = 0
+    building_df.loc[:,"number_of_buildings_with_HP_ground"] = 0
+    building_df.loc[:,"number_of_buildings_with_HP_air"] = 0
+    
+    # add country to building_df:
+    building_df.loc[:,"country"] = country
     
     for index, row in building_df.iterrows():
         number_of_buildings = building_segment.loc[
@@ -121,9 +193,29 @@ for country in country_list:
         building_df.loc[index, "number_of_buildings"] = total_number_of_buildings
         
         # filter out number of heat pumps 
+        # Air:
+        number_of_air_hp = building_segment.loc[
+            (building_segment.loc[:, "building_classes_index"] == row["index"]) &
+            (building_segment.loc[:, "heat_supply_system_index"] == 42)]
+        total_number_of_air_hp = number_of_air_hp.loc[:, "number_of_buildings"].sum()
+        building_df.loc[index, "number_of_buildings_with_HP_air"] = total_number_of_air_hp
+        
+        # Ground:
+        number_of_ground_hp = building_segment.loc[
+            (building_segment.loc[:, "building_classes_index"] == row["index"]) & 
+            (building_segment.loc[:, "heat_supply_system_index"] == 43)]
+        total_number_of_ground_hp = number_of_ground_hp.loc[:, "number_of_buildings"].sum() 
+        building_df.loc[index, "number_of_buildings_with_HP_ground"] = total_number_of_ground_hp
+        
+    # append the df to the big df
+    df_all_countries = df_all_countries.append(building_df)
     
+           
+    print(f"added {country}")
+df_all_countries = df_all_countries.reset_index(drop=True).rename(columns={"index": "invert_index"})
+ 
+df_all_countries.to_json(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\SFH_building_data.json", orient="table")
 
-    
    
 
 
