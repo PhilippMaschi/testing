9eb3e52ed9dc304606667382045b3c145f57ba46
PhilippMaschi <phil.masch@hotmail.com>
2022-03-08 13:39:31 +0100

    synthetic load profiles converted to json

--- excel_to_json.py
+++ excel_to_json.py
@@ -6,6 +6,8 @@ Created on Thu Feb 10 12:11:28 2022
 """
 import pandas as pd
 import numpy as np
+import pyodbc
+from pathlib import Path
 
 
 path_to_file = r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\inputdata\NUTS2021.xlsx"
@@ -38,15 +40,58 @@ nuts2_frame = pd.DataFrame(np.column_stack([nuts2_country_column, NUTS2]), colum
 nuts3_frame = pd.DataFrame(np.column_stack([nuts3_country_column, NUTS3]), columns=["country", "nuts_id"])
 
 
-# frames to json:
-nuts1_frame.to_json(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\NUTS1.json", orient="table")
-nuts2_frame.to_json(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\NUTS2.json", orient="table")
-nuts3_frame.to_json(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\NUTS3.json", orient="table")
+# # frames to json:
+# nuts1_frame.to_json(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\NUTS1.json", orient="table")
+# nuts2_frame.to_json(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\NUTS2.json", orient="table")
+# nuts3_frame.to_json(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\NUTS3.json", orient="table")
 
 
 
-path_to_file_heat_demand = r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\inputdata\heat_demand_nuts_3.csv"
+# path_to_file_heat_demand = r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\inputdata\heat_demand_nuts_3.csv"
 
-heat_demand_df = pd.read_csv(path_to_file_heat_demand)
-heat_demand_df.to_json(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\heat_demand.json", orient="table")
+# heat_demand_df = pd.read_csv(path_to_file_heat_demand)
+# heat_demand_df.to_json(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\heat_demand.json", orient="table")
+
+# path_to_floor_area = r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\inputdata\ground_floor_area_nuts_3.csv"
+# ground_floor_area = pd.read_csv(path_to_floor_area)
+
+#%%
+path_to_synth_load = Path(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\inputdata\synthetic_profiles")
+synth_load_df = pd.DataFrame()
+for child in path_to_synth_load.iterdir():
+    for file in child.iterdir():
+        if file.suffix == ".xlsx":
+            df = pd.read_excel(file, sheet_name="Profile").set_index("Unnamed: 0")
+            household_profile = pd.to_numeric(df.loc[:, "H0"].drop("ts [UTC]"))
+            household_profile.index = pd.to_datetime(household_profile.index)
+            household_profile = household_profile.resample("1H").sum()
+            if len(household_profile) == 8785:  # leap year (drop 29th february)
+                household_profile = household_profile[~((household_profile.index.month == 2) &
+                                                        (household_profile.index.day == 29))]
+            household_profile = household_profile.reset_index(drop=True).drop(0)  # drop first row (23:00-24:00 last year)
+            household_profile = household_profile.to_numpy() * 1_000  # from kWh in Wh
+            year = file.stem[-4:]
+            synth_load_df[year] = household_profile
+
+# save file to json
+synth_load_df.to_json(r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\synthetic_load_household.json", orient="table")
+
+
+
+
+
+#%%
+
+# path_to_synth_load = r"C:\Users\mascherbauer\PycharmProjects\NewTrends\Prosumager\_Refactor\data\inputdata\synthetic_profiles\synthload2017\SynthLoad2017.mdb"
+# driver = "{Microsoft Access Driver (*.mdb)}"
+# PWD = ""
+
+# con = pyodbc.connect('DRIVER={};DBQ={};PWD={}'.format(driver,path_to_synth_load,PWD))
+# cur = con.cursor()
+
+# SQL = 'SELECT * FROM all_tables;'
+# rows = cur.execute(SQL).fetchall()
+
+
+
 
