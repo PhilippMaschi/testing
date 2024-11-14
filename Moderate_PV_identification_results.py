9e4fbc0897bb431ef8349c7047cd6b126d335cc6
PhilippMaschi <phil.masch@hotmail.com>
2024-11-14 18:29:31 +0100

    bozen results

--- Moderate_PV_identification_results.py
+++ Moderate_PV_identification_results.py
@@ -19,8 +19,19 @@ from matplotlib.lines import Line2D
 import tqdm
 import time
 from PIL import Image
+import shutil
 
 
+# BOZEN
+BOZEN_CLASSIFIER_RESULTS = pd.read_csv(Path(r"/home/users/pmascherbauer/projects4/workspace_philippm/Bozen/building-stock-analysis/T3.1-dynamic-analysis/Case-study-II-III-PV-analysis/solar-panel-classifier/new_data/") / "Classifier_Results.csv", sep=";")
+
+with open(Path(r"/home/users/pmascherbauer/projects4/workspace_philippm/Bozen/building-stock-analysis/T3.1-dynamic-analysis/Case-study-II-III-PV-analysis") / "OSM_IDs_lat_lon.json", "r") as f:
+    BOZEN_BUILDING_LOCS = json.load(f)
+
+BOZEN_MANUALLY_IDENTIFIED = pd.read_csv(Path(r"/home/users/pmascherbauer/projects4/workspace_philippm/Bozen/building-stock-analysis/T3.1-dynamic-analysis/Case-study-II-III-PV-analysis/") / "OSM_IDs_labeled.csv", sep=";")
+
+
+# Valencia
 PATH_2_INPUT_TIFS = Path(r"/home/users/pmascherbauer/projects4/workspace_philippm/building-stock-analysis/T3.1-dynamic-analysis/Case-study-II-III-PV-analysis/solar-panel-classifier/new_data/input_tifs/")
 CLASSIFIER_RESULTS = pd.read_csv(Path(r"/home/users/pmascherbauer/projects4/workspace_philippm/building-stock-analysis/T3.1-dynamic-analysis/Case-study-II-III-PV-analysis/solar-panel-classifier/new_data/") / "Classifier_Results.csv", sep=";")
 with open(Path(r"/home/users/pmascherbauer/projects4/workspace_philippm/building-stock-analysis/T3.1-dynamic-analysis/Case-study-II-III-PV-analysis") / "OSM_IDs_lat_lon.json", "r") as f:
@@ -29,9 +40,10 @@ with open(Path(r"/home/users/pmascherbauer/projects4/workspace_philippm/building
 VALENCIA_DATA = pd.read_csv(Path(__file__).parent / "datos-de-autoconsumo-de-energias-renovables.csv", sep=";", encoding="ISO-8859-1")
 VALENCIA_DATA.loc[VALENCIA_DATA["CODIGO POSTAL"].isna(), "CODIGO POSTAL"] = VALENCIA_DATA.loc[VALENCIA_DATA["CODIGO POSTAL"].isna(), "CODIGO MUNICIPIO"]
 VALENCIA_DATA.drop_duplicates(keep="first", inplace=True)
+VALENCIA_DATA = VALENCIA_DATA.loc[VALENCIA_DATA["COMBUSTIBLE"].isna(), :].copy()
 
-# PATH_2_INPUT_TIFS = Path(r"X:\projects4\workspace_philippm\building-stock-analysis\T3.1-dynamic-analysis\Case-study-II-III-PV-analysis\solar-panel-classifier\new_data\input_tifs")
-# BUILDING_LOCS = {"1000248182": "39.345798,-0.571910"}
+PATH_2_INPUT_TIFS = Path(r"X:\projects4\workspace_philippm\building-stock-analysis\T3.1-dynamic-analysis\Case-study-II-III-PV-analysis\solar-panel-classifier\new_data\input_tifs")
+BUILDING_LOCS = {"1000248182": "39.345798,-0.571910"}
 
 MANUALLY_IDENTIFIED = pd.read_csv(Path(r"/home/users/pmascherbauer/projects4/workspace_philippm/building-stock-analysis/T3.1-dynamic-analysis/Case-study-II-III-PV-analysis/") / "OSM_IDs_labeled.csv", sep=";")
 
@@ -52,25 +64,25 @@ def split_id_name(name: str):
     return osmid, hash
 
 
-def get_input_tif_from_hash(hash: str) -> Path:
+def get_input_tif_from_hash(hash: str, ) -> Path:
     input_tifs = [f for f in PATH_2_INPUT_TIFS.iterdir() if f.suffix == ".tif"]
     hashes = [generate_hash(f.name) for f in input_tifs]
     index = hashes.index(hash)
     return input_tifs[index]
 
 
-def select_all_buildings_with_specific_hash(hash: str):
-    return CLASSIFIER_RESULTS.loc[CLASSIFIER_RESULTS["OSM_ID"].str.contains(hash), :]
+def select_all_buildings_with_specific_hash(classifier_result: str):
+    return classifier_result.loc[classifier_result["OSM_ID"].str.contains(hash), :]
 
 
-def find_adress_and_post_codes():
-    identified = [name.split("_")[1] for name in CLASSIFIER_RESULTS.loc[CLASSIFIER_RESULTS["prediction"]==1, "OSM_ID"]]
+def find_adress_and_post_codes(classifier_result, building_locs):
+    identified = [name.split("_")[1] for name in classifier_result.loc[classifier_result["prediction"]==1, "OSM_ID"]]
     post_codes = {}
     villages = {}
     geolocator = Nominatim(user_agent="my_geocoder")
 
     for osmid in tqdm.tqdm(identified):
-        lat_lon_str = BUILDING_LOCS[osmid]
+        lat_lon_str = building_locs[osmid]
         lat, lon = float(lat_lon_str.split(",")[0]), float(lat_lon_str.split(",")[1])
 
         location = geolocator.reverse((lat, lon), addressdetails=True)
@@ -87,7 +99,7 @@ def find_adress_and_post_codes():
     df = pd.DataFrame(villages, post_codes)
 
 
-def number_of_PVs_per_region(gdf_3035) -> pd.DataFrame:
+def number_of_PVs_per_region_valencia(gdf_3035) -> pd.DataFrame:
     path_rg = Path(r"nuts_json") / "NUTS_RG_01M_2021_3035_LEVL_2.json"
     gdf_rg = gpd.read_file(path_rg)
     valencia = gdf_rg[gdf_rg["NAME_LATN"]=="Comunitat Valenciana"].copy()
@@ -123,7 +135,7 @@ def number_of_PVs_per_region(gdf_3035) -> pd.DataFrame:
     return valencia_numbers
 
 def plot_identified_numbers(gdf_3035):
-    numbers_df = number_of_PVs_per_region(gdf_3035)
+    numbers_df = number_of_PVs_per_region_valencia(gdf_3035)
     
     plot_df = numbers_df.melt(id_vars=["PROVINCIA", "identified number of PVs in (%)"], value_name="number", var_name="source")
     fig, ax = plt.subplots(figsize=(20, 15))#, subplot_kw={'projection': cartopy.crs.epsg(3035)})
@@ -142,20 +154,36 @@ def plot_identified_numbers(gdf_3035):
     plt.savefig(Path(__file__).parent / "MODERATE_T3.4" / "Identified_Numbers.png", dpi=600)
     plt.close()
 
+def load_LAU_data():
+    path2data = Path(r"/home/users/pmascherbauer/projects4/workspace_philippm/testing/MODERATE_T3.4/") / "LAU_RG_01M_2021_3035.shp"
+    gdf = gpd.read_file(path2data)
+    spain_lau = gdf[gdf["CNTR_CODE"]=="ES"]
 
-def plot_pvs_on_map(gdf_3035, gdf_3035_all):
+
+    VALENCIA_DATA.drop(columns=["FECHA RECHAZO", "OBSERVACIONES RECHAZO", "FECHA INSCRIPCIÃN", "MODALIDAD", "EMPRESA DISTRIBUIDORA",
+                                "TENSIÃN PUNTO CONEXIÃN", "INDIVIDUAL/COLECTIVO", "NÃMERO CONSUMIDORES", "EMPRESA DISTRIBUIDORA INST", "ESQUEMA MEDIDA", 
+                                "FECHA COMUNICACIÃN DISTRIBUIDORA", "CÃDIGO RADNE", "SUBTIPO TECNOLOGÃA", "TIPO INSTALACIÃN", "TENSIÃN GENERACIÃN",
+                                "SSAA", "CÃDIGO PRETOR", "CÃDIGO RAIPRE", "CÃDIGO ALMACENAMIENTO RADNE", "ENERGÃA MÃX ALMACENABLE",
+                                "CÃDIGO REGISTRO AUTONÃMICO", "COMPENSACIÃN", "POBLACIÃN INST", "ESTADO", "USO", "SUELO URBANO", "POTENCIA SALIDA", "POBLACIÃN"], inplace=True)
+
+    pass
+
+
+def plot_pvs_on_map(gdf_3035, gdf_3035_all, region: str):
     # load valencia boundaries:
     path_rg = Path(r"nuts_json") / "NUTS_RG_01M_2021_3035_LEVL_2.json"
     gdf_rg = gpd.read_file(path_rg)
-    valencia = gdf_rg[gdf_rg["NAME_LATN"]=="Comunitat Valenciana"].copy()
-    path_rg3 = Path(r"nuts_json") / "NUTS_RG_01M_2021_3035_LEVL_3.json"
-    gdf_rg3 = gpd.read_file(path_rg3)
-    spain = gdf_rg3[gdf_rg3["CNTR_CODE"]=="ES"].copy()
-    valencia3 = spain[spain.within(valencia.geometry.iloc[0])]
+    if region == "valencia":
+        nuts3_region = gdf_rg[gdf_rg["NAME_LATN"]=="Comunitat Valenciana"].copy()
+
+    else:
+        nuts3_region = gdf_rg[gdf_rg["NAME_LATN"]=="Provincia Autonoma di Bolzano/Bozen"].copy()
+
+
     # plot the identified PVs on the map 
     fig, axes = plt.subplots(ncols=2, nrows=1, figsize=(20, 15))#, subplot_kw={'projection': cartopy.crs.epsg(3035)})
-    valencia3.plot(ax=axes[0], color="lightgrey")
-    valencia3.plot(ax=axes[1], color="lightgrey")
+    nuts3_region.plot(ax=axes[0], color="lightgrey")
+    nuts3_region.plot(ax=axes[1], color="lightgrey")
     gdf_3035_all.plot(ax=axes[1], color="blue", markersize=0.5, alpha=0.5)
     gdf_3035.plot(ax=axes[0], color="red", markersize=0.5, alpha=0.5)
     legend_elements = [
@@ -169,15 +197,15 @@ def plot_pvs_on_map(gdf_3035, gdf_3035_all):
         ax.set_xticks([])  # Remove x-axis ticks
         ax.set_yticks([])
     plt.tight_layout()
-    plt.savefig(Path(__file__).parent / "MODERATE_T3.4" / "Valencia.png", dpi=1200)
+    plt.savefig(Path(__file__).parent / "MODERATE_T3.4" / f"{region}.png", dpi=600)
     plt.close()
 
 
 def main():
 
-    input_tifs = [f for f in PATH_2_INPUT_TIFS.iterdir() if f.suffix == ".tif"]
-    hashes = [generate_hash(f.name) for f in input_tifs]
-    regions = {}
+    # input_tifs = [f for f in PATH_2_INPUT_TIFS.iterdir() if f.suffix == ".tif"]
+    # hashes = [generate_hash(f.name) for f in input_tifs]
+    # regions = {}
     # for i, hash in enumerate(hashes):
     #     tif = input_tifs[i]
     #     df = select_all_buildings_with_specific_hash(hash)
@@ -255,12 +283,59 @@ def main():
     )
     gdf_3035_all = gdf_4326_all.to_crs("EPSG:3035")
 
-    plot_pvs_on_map(gdf_3035, gdf_3035_all)
+    plot_pvs_on_map(gdf_3035, gdf_3035_all, region="valencia")
+
+def main_bozen():
 
 
+    identified = [name.replace("building_", "") for name in BOZEN_CLASSIFIER_RESULTS.loc[BOZEN_CLASSIFIER_RESULTS["prediction"]==1, "OSM_ID"]]
+    all_oms = [name.replace("building_", "") for name in BOZEN_CLASSIFIER_RESULTS.loc[:, "OSM_ID"]]
+    # all_hashs = ["".join(name.replace("building_", "").split("_")[1:]) for name in CLASSIFIER_RESULTS.loc[:, "OSM_ID"]]
+
+    # wrong = []
+    # for i in MANUALLY_IDENTIFIED.loc[MANUALLY_IDENTIFIED["has_pv"]=="yes", "osmid"]:
+    #     if str(i) not in identified:
+    #         wrong.append(i)
+    #     if str(i) not in all_oms:
+    #         print("FUCK")
+    
+
+
+    lats_pv = []
+    lons_pv = []
+    lats = []
+    lons = []
+    for osmid, lat_lon_str in BOZEN_BUILDING_LOCS.items():
+        lat, lon = float(lat_lon_str.split(",")[0]), float(lat_lon_str.split(",")[1])
+        lats.append(lat)
+        lons.append(lon)
+        if osmid in identified:
+            lats_pv.append(lat)
+            lons_pv.append(lon)
+
+    gdf_4326 = gpd.GeoDataFrame(
+        {'latitude': lats_pv, 'longitude': lons_pv},
+        geometry=[shapely.geometry.Point(lon, lat) for lon, lat in zip(lons_pv, lats_pv)],
+        crs="EPSG:4326"  # Original coordinate reference system
+    )
+    gdf_3035 = gdf_4326.to_crs("EPSG:3035")
+
+    # show how many PVs were identified in each NUTS3 region:
+    # plot_identified_numbers(gdf_3035=gdf_3035)
+
+
+    gdf_4326_all = gpd.GeoDataFrame(
+        {'latitude': lats, 'longitude': lons},
+        geometry=[shapely.geometry.Point(lon, lat) for lon, lat in zip(lons, lats)],
+        crs="EPSG:4326"  # Original coordinate reference system
+    )
+    gdf_3035_all = gdf_4326_all.to_crs("EPSG:3035")
+
+    plot_pvs_on_map(gdf_3035, gdf_3035_all, region="bozen")
 
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    main_bozen()
 
