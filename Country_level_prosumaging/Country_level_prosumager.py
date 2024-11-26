1e482ca05dcc89e1395036e6b3acda561906541f
PhilippMaschi <phil.masch@hotmail.com>
2024-11-26 15:34:13 +0100

    create parquet file for country load profiles

--- Country_level_prosumaging/Country_level_prosumager.py
+++ Country_level_prosumaging/Country_level_prosumager.py
@@ -106,31 +106,31 @@ def flexibility_factor_hourly(consumer_profile: np.array, prosumager_profile: np
 def supply_and_demand_matching(price_profile: np.array, consumer_profile: np.array, prosumager_profile: np.array) -> dict:
     """set price threshold and if the price is below that threshold the used electricity is “renewable”. Check how much more “renewable” electricity can be used
     returns the differnce betwen prosumager demand at low prices and consumer demand at low prices"""
-    match_increase = {}
-    match_decrease = {}
-    for quantile in np.arange(0, 1.05, 0.05):
-        price_threshold = np.quantile(price_profile, quantile)
-        consumer_match = 0
-        prosumager_match = 0
-        for i, price in enumerate(price_profile):
-            if price <= price_threshold:
-                diff =  prosumager_profile[i] - consumer_profile[i]
-                if diff < 0:
-                    match_decrease
+    # match_increase = {}
+    # match_decrease = {}
+    # for quantile in np.arange(0, 1.05, 0.05):
+    #     price_threshold = np.quantile(price_profile, quantile)
+    #     consumer_match = 0
+    #     prosumager_match = 0
+    #     for i, price in enumerate(price_profile):
+    #         if price <= price_threshold:
+    #             diff =  prosumager_profile[i] - consumer_profile[i]
+    #             if diff < 0:
+    #                 match_decrease
                 
         
-        diff = prosumager_match - consumer_match
-        diff[diff<0] = 0
+    #     diff = prosumager_match - consumer_match
+    #     diff[diff<0] = 0
 
     matching = []
     for i, price in enumerate(price_profile):
         diff =  prosumager_profile[i] - consumer_profile[i]
         matching.append(diff)
-    x = pd.DataFrame({"price": price_profile, "change in electricity demand": matching})
-    x.reset_index(drop=True).to_csv(Path(__file__).parent / "single_country_plots"/"change_demand.csv", index=False, sep=";")
+    df = pd.DataFrame({"price": price_profile, "change in electricity demand": matching})
+    df.reset_index(drop=True, inplace=True)
     # match[price_threshold * 1_000] = diff
 
-    return match 
+    return df 
 
 def flexible_storage_efficiency(consumer_profile: np.array, prosumager_profile: np.array) -> float:
     """calculates the upward storage efficiency between 0 and 1"""
@@ -269,26 +269,33 @@ def get_country_load_profiles(folder_name: Path, percentage_dhw_tanks: float, pe
     hp_df = building_table.loc[building_table["number_buildings_electricity"] == 0, :]
 
     filtered_scenarios = scenario_table.loc[scenario_table["ID_Building"].isin(list(hp_df.ID_Building)), :]
+    # in some countries there are not sufficient heat pumps installed in 2020 (BGR 2020) according to INVERT: 
+    # return 0 for those..
+    if filtered_scenarios.empty:
+        return pd.DataFrame.from_dict({"opt_grid_demand_stock_MW": np.zeros(8760), 
+                                       "ref_grid_demand_stock_MW": np.zeros(8760),
+                                       "Hour": np.arange(1, 8761)},  orient="index").T
+    else:
 
-    scenario_df = define_tech_scenario(
-        prob_dhw=percentage_dhw_tanks,
-        prob_buffer=percentage_buffer_tanks,
-        df_scenarios=filtered_scenarios,
-        df_hp=hp_df
-    )
+        scenario_df = define_tech_scenario(
+            prob_dhw=percentage_dhw_tanks,
+            prob_buffer=percentage_buffer_tanks,
+            df_scenarios=filtered_scenarios,
+            df_hp=hp_df
+        )
 
-    opt_loads, ref_loads = load_electricity_demand_profiles(scenario_ids=list(scenario_df["ID_Scenario"]), folder=folder_name)
+        opt_loads, ref_loads = load_electricity_demand_profiles(scenario_ids=list(scenario_df["ID_Scenario"]), folder=folder_name)
 
-    # merge the numbers of each building scenario with the opt and ref loads:
-    building_numbers = scenario_df.loc[:, ["ID_Scenario", "number_buildings"]].copy().set_index("ID_Scenario").sort_index()
-    opt_stock = opt_loads.sort_values(by=["ID_Scenario", "Hour"]).set_index("ID_Scenario").join(building_numbers)
-    opt_stock["opt_grid_demand_stock_MW"] = opt_stock["Grid"] * opt_stock["number_buildings"] / 1_000_000  # MW
-    ref_stock = ref_loads.sort_values(by=["ID_Scenario", "Hour"]).set_index("ID_Scenario").join(building_numbers)
-    ref_stock["ref_grid_demand_stock_MW"] = ref_stock["Grid"] * ref_stock["number_buildings"] / 1_000_000  # MW
+        # merge the numbers of each building scenario with the opt and ref loads:
+        building_numbers = scenario_df.loc[:, ["ID_Scenario", "number_buildings"]].copy().set_index("ID_Scenario").sort_index()
+        opt_stock = opt_loads.sort_values(by=["ID_Scenario", "Hour"]).set_index("ID_Scenario").join(building_numbers)
+        opt_stock["opt_grid_demand_stock_MW"] = opt_stock["Grid"] * opt_stock["number_buildings"] / 1_000_000  # MW
+        ref_stock = ref_loads.sort_values(by=["ID_Scenario", "Hour"]).set_index("ID_Scenario").join(building_numbers)
+        ref_stock["ref_grid_demand_stock_MW"] = ref_stock["Grid"] * ref_stock["number_buildings"] / 1_000_000  # MW
 
-    country_load_df = pd.concat([opt_stock, ref_stock["ref_grid_demand_stock_MW"]], axis=1)
+        country_load_df = pd.concat([opt_stock, ref_stock["ref_grid_demand_stock_MW"]], axis=1)
 
-    return country_load_df
+        return country_load_df
 
 def get_national_demand_profiles():
     gen_file = Path(__file__).parent.parent / "ENTSOE Generation" / "ENTSOE_generation_MWh_2019.csv"
@@ -300,62 +307,106 @@ def get_price_profile(folder_name: Path):
     price_table = db.read_dataframe(table_name="OperationScenario_EnergyPrice", column_names=["electricity_1"])
     return price_table
 
-def main(percentage_dhw_tanks: float, percentage_buffer_tanks: float):
+def create_national_demand_profiles_parquet(percentage_dhw_tanks: float, percentage_buffer_tanks: float, parquet_file: Path):
     path_2_model_results = Path(r"/home/users/pmascherbauer/projects/Philipp/PycharmProjects/data/output/")
-    folder_names = [f"D5.4_{country}_{year}" for country in list(EUROPEAN_COUNTRIES.keys()) for year in [2020, 2030, 2040, 2050]]
-    folder_names_low_scenario = [f"D5.4_low_{country}_{year}" for country in list(EUROPEAN_COUNTRIES.keys()) for year in [2020, 2030, 2040, 2050]] 
+    
+    if not parquet_file.exists():
+        folder_names = [f"D5.4_{country}_{year}" for country in list(EUROPEAN_COUNTRIES.keys()) for year in [2020, 2030, 2040, 2050]]
+        big_df = pd.DataFrame()
 
+        for folder_name in folder_names:
+            if folder_name in ["D5.4_CYP_2020", "D5.4_MLT_2020"]:
+                continue
+            folder = path_2_model_results / folder_name
+            country = folder.name.split("_")[-2]
+            year = folder.name.split("_")[-1]
+            print(f"analysing {country} {year}")
+
+            country_loads = get_country_load_profiles(
+                folder_name=folder,
+                percentage_dhw_tanks=percentage_dhw_tanks,
+                percentage_buffer_tanks=percentage_buffer_tanks
+            )
+            # add the country loads from different building types together:
+            consumers_MW = np.array(country_loads.groupby(["Hour"]).sum()["ref_grid_demand_stock_MW"])
+            prosumer_MW = np.array(country_loads.groupby(["Hour"]).sum()["opt_grid_demand_stock_MW"])
+
+            price = np.array(get_price_profile(folder).iloc[:, 0])
+
+            big_df.loc[:, f"{country}_{year}_ref_load_MW"] = consumers_MW
+            big_df.loc[:, f"{country}_{year}_opt_load_MW"] = prosumer_MW
+            big_df.loc[:, f"{country}_{year}_price"] = price
+        
+        big_df.to_parquet(parquet_file)
+
+    # same for low scenario:
+    levethian_parquet_file = parquet_file.parent / parquet_file.name.replace(".parquet.gzip", "_low.parquet.gzip")
+    if not levethian_parquet_file.exists():
+        folder_names_low_scenario = [f"D5.4_low_{country}_{year}" for country in list(EUROPEAN_COUNTRIES.keys()) for year in [2020, 2030, 2040, 2050]] 
+        low_big_df = pd.DataFrame()
+
+        for folder_name in folder_names_low_scenario:
+            if folder_name in ["D5.4_low_CYP_2020", "D5.4_low_MLT_2020"]:
+                continue
+            folder = path_2_model_results / folder_name
+            country = folder.name.split("_")[-2]
+            year = folder.name.split("_")[-1]
+            print(f"analysing {country} {year}")
+
+            country_loads = get_country_load_profiles(
+                folder_name=folder,
+                percentage_dhw_tanks=percentage_dhw_tanks,
+                percentage_buffer_tanks=percentage_buffer_tanks
+            )
+            # add the country loads from different building types together:
+            consumers_MW = np.array(country_loads.groupby(["Hour"]).sum()["ref_grid_demand_stock_MW"])
+            prosumer_MW = np.array(country_loads.groupby(["Hour"]).sum()["opt_grid_demand_stock_MW"])
+
+            price = np.array(get_price_profile(folder).iloc[:, 0])
+
+            low_big_df.loc[:, f"{country}_{year}_ref_load_MW"] = consumers_MW
+            low_big_df.loc[:, f"{country}_{year}_opt_load_MW"] = prosumer_MW
+            low_big_df.loc[:, f"{country}_{year}_price"] = price
+        
+
+        low_big_df.to_parquet(levethian_parquet_file)
+
+
+def main(percentage_dhw_tanks: float, percentage_buffer_tanks: float):
+    parquet_file = Path(__file__).parent / f"EU27_loads_dhw-{percentage_dhw_tanks}_buffer-{percentage_buffer_tanks}.parquet.gzip"
+
+    create_national_demand_profiles_parquet(percentage_dhw_tanks, percentage_buffer_tanks, parquet_file)
+
+    df = pd.read_parquet(Path(__file__).parent / f"EU27_loads_dhw-{percentage_dhw_tanks}_buffer-{percentage_buffer_tanks}.parquet.gzip")
     national_demand = get_national_demand_profiles()
 
-    flexiblity_factor = {}
-    flexiblity_factor_hourly = {}
-    s_and_d_matching_dict = {}
-    storage_efficiency = {}
+    # calculate the factors:
+    # flexiblity_factor[f"{country}_{year}"] = flexibility_factor(
+    #     consumer_profile=consumers_MW, 
+    #     prosumager_profile=prosumer_MW, 
+    #     national_demand_profile=np.array(national_demand[COUNTRY_CODES[country]])
+    # )
+    # flexiblity_factor_hourly[f"{country}_{year}"] = flexibility_factor_hourly(
+    #     consumer_profile=consumers_MW,
+    #     prosumager_profile=prosumer_MW,
+    #     national_demand_profile=np.array(national_demand[COUNTRY_CODES[country]])
+    # )
 
-    folder_names = folder_names[:4]
-    for folder_name in folder_names:
-        folder = path_2_model_results / folder_name
-        country = folder.name.split("_")[-2]
-        year = folder.name.split("_")[-1]
-        print(f"analysing {country} {year}")
+    # s_and_d_matching_dict[f"{country}_{year}"] = supply_and_demand_matching(
+    #     price_profile=price,
+    #     consumer_profile=consumers_MW,
+    #     prosumager_profile=prosumer_MW
+    # )
 
-        country_loads = get_country_load_profiles(
-            folder_name=folder,
-            percentage_dhw_tanks=percentage_dhw_tanks,
-            percentage_buffer_tanks=percentage_buffer_tanks
-        )
-        # add the country loads from different building types together:
-        consumers_MW = np.array(country_loads.groupby(["Hour"]).sum()["ref_grid_demand_stock_MW"])
-        prosumer_MW = np.array(country_loads.groupby(["Hour"]).sum()["opt_grid_demand_stock_MW"])
+    # storage_efficiency[f"{country}_{year}"] = flexible_storage_efficiency(
+    #     consumer_profile=consumers_MW,
+    #     prosumager_profile=prosumer_MW
+    # )
 
-        price = np.array(get_price_profile(folder).iloc[:, 0])
-        # calculate the factors:
-        flexiblity_factor[f"{country}_{year}"] = flexibility_factor(
-            consumer_profile=consumers_MW, 
-            prosumager_profile=prosumer_MW, 
-            national_demand_profile=np.array(national_demand[COUNTRY_CODES[country]])
-        )
-        flexiblity_factor_hourly[f"{country}_{year}"] = flexibility_factor_hourly(
-            consumer_profile=consumers_MW,
-            prosumager_profile=prosumer_MW,
-            national_demand_profile=np.array(national_demand[COUNTRY_CODES[country]])
-        )
-
-        s_and_d_matching_dict[f"{country}_{year}"] = supply_and_demand_matching(
-            price_profile=price,
-            consumer_profile=consumers_MW,
-            prosumager_profile=prosumer_MW
-        )
-
-        storage_efficiency[f"{country}_{year}"] = flexible_storage_efficiency(
-            consumer_profile=consumers_MW,
-            prosumager_profile=prosumer_MW
-        )
-
-        plot_supply_and_demand_matching_over_price(price=price,
-                                                   s_d_match=s_and_d_matching_dict[f"{country}_{year}"],
-                                                   country=country,
-                                                   year=year)
+    # plot_supply_and_demand_matching_over_price(price=price,
+    #                                             s_d_match=s_and_d_matching_dict[f"{country}_{year}"],
+    #                                             country=country,
+    #                                             year=year)
 
 if __name__ == "__main__":
     main(
--- Country_level_prosumaging/country_level_prosumaging_figures.py
+++ Country_level_prosumaging/country_level_prosumaging_figures.py
@@ -5,27 +5,61 @@ import seaborn as sns
 from pathlib import Path
 import sqlalchemy
 from typing import List
+import matplotlib.gridspec as gridspec
 
 
 
 
-def plot_supply_and_demand_matching_over_price(price: np.array, s_d_match: dict, country: str, year: str):
+def plot_supply_and_demand_matching_over_price(price: np.array, s_d_match: pd.DataFrame, country: str, year: str):
+    bin_size = 20
+    df = s_d_match.copy()
+    df["price"] = df["price"] * 1000  # cent/kWh
 
-    plt.plot(s_d_match.keys(), np.array([s for s in s_d_match.values()]) / 1_000)
-    ax = plt.gca()
-    ax2 = ax.twinx()
-    ax2.hist(price*1_000, 100, alpha=0.5, color="green")
-    ax.set_xlabel("electricity wholesale price (cent/kWh)")
-    ax.set_ylabel("cumulated additional electricity usage below \n a price threshold (GWh)")
-    ax2.set_ylabel("frequency of prices occuring during the year")
-    ax2.tick_params(axis='y', colors='green')
-    ax2.spines['right'].set_color('green')
-    plt.savefig(Path(__file__).parent / "single_country_plots" / f"supply_demand_matching_{country}_{year}.png")
+    df["price_bins"] = pd.cut(df['price'], bins=bin_size)
+    df['price_bins_str'] = df['price_bins'].apply(lambda x: f"[{x.left:.1f}, {x.right:.1f}]")
+
+    for price_bin, group in df.groupby("price_bins"):
+        df.loc[group.index, "price frequency"] = len(group)
+        df.loc[group.index, "cumulative energy"] = group["change in electricity demand"].sum()
+
+    df["cumulative energy"] = df["cumulative energy"] / 1_000  # GWh
+
+        
+
+    fig = plt.figure(constrained_layout=True)
+    gs = gridspec.GridSpec(2, 1, height_ratios=[1, 3], figure=fig)  # Two rows, stacked vertically with height ratios
+
+    ax2 = fig.add_subplot(gs[0])
+    sns.barplot(
+        data=df,
+        x="price_bins_str",
+        y="price frequency",
+        ax=ax2,
+        color="green",
+        alpha=0.5
+    )
+    ax2.set_ylabel("Price frequency")
+    ax2.spines['bottom'].set_visible(False)
+    ax2.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)  # Hide x-tick labels on top plot
+    ax2.set_xlabel("")
+
+    ax = fig.add_subplot(gs[1])
+    sns.lineplot(
+        data=df,
+        x="price_bins_str",
+        y="cumulative energy",
+        ax=ax,
+    )
+    ax.set_ylabel("cumulative shifted electricity (GWh)")
+    ax.hlines(y=0, xmin=ax.get_xlim()[0], xmax=ax.get_xlim()[1], color="black")
+    ax.set_xlabel("Wholesale electricity price (cent/kWh)")
+    plt.xticks(rotation=45)
+    ax.set_xlim(left=0, right=bin_size)
+
+    # Save the figure
+    output_path = Path(__file__).parent / "single_country_plots" / f"Energy_over_price_{country}_{year}.png"
+    plt.tight_layout()
+    plt.savefig(output_path)
+
+    # Close the plot to free up memory
     plt.close()
-
-
-    plt.scatter(x["price"] * 1_000, x["change in electricity demand"])
-    plt.savefig(Path(__file__).parent / "single_country_plots" / f"supply_demand_matching_{country}_{year}.png")
-    plt.close()
-
-
